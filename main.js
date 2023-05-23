5273c66a79e69bc0dbdb89f4f720edaf5dc42b5b
umitrock <rock4software@gmail.com>
2023-05-23 12:45:28 +0300

    Popup Box Main

---
+++ main.js
@@ -0,0 +1,8 @@
+    let popup = document.querySelector(".pop-up");
+
+    function OpnePopUpBox(){
+      popup.classList.add("open-PopUp");
+    }
+    function ClosePopUpBox(){
+      popup.classList.remove("open-PopUp");
+    }
